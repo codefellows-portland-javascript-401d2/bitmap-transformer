bf1f9f90f9d03f6cde2872d6de485e7671b2f4f2
Allison Davis <a.rae.davis@gmail.com>
2016-05-12 15:31:20 -0700

    add project files

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+describe('bitmap-transformer', () => {
+  it('reads a file and returns a buffer')
+
+});
+//module reads a file from disk
+    //read file into buffer
+    //convert header data to JS object
+    //pass buffer to transform callback
+
+
+//transform function(s)
+
+
+
+//writes buffer to new file
