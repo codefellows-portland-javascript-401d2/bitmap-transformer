df9ed96b6afbcd00d0c7cccce47095bb2670c83d
DonChatelain <djchatelain@gmail.com>
2016-05-13 15:56:09 -0700

    converts palette decimals back to buffer

--- tests/bitmap.test.js
+++ tests/bitmap.test.js
@@ -11,13 +11,6 @@ it('targets image file and stores decimal array in var', () => {
   getBytes('palette-bitmap.bmp', function(result) {
 
     console.log(result);
-    var decimalValues = [];
-    result.forEach( (decimal, index) => {
-      decimalValues.push(index + ' ' + decimal);
-    });
-    for (var i = 54; i < 182; i++) {
-      console.log(decimalValues[i]);
-    }
     assert(result);
   });
 
@@ -28,11 +21,26 @@ function getBytes(file, callback) {
   fs.readFile('./images/' + file, (err, buf) => {
     if (err) throw err;
 
-    const offset = buf.readInt16LE(10);
-    const headData = Buffer.from(buf.buffer, 0, offset);
-    const bodyData = Buffer.from(buf.buffer, offset);
-
-    // callback(headData);
+    const paletteStart = 54;
+    const headEnd = buf.readInt16LE(10);
+    const headBuffer = Buffer.from(buf.buffer, 0, paletteStart);
+    const paletteBuffer = Buffer.from(buf.buffer, paletteStart, headEnd);
+    const bodyBuffer = Buffer.from(buf.buffer, headEnd);
+    const paletteDecimals = [];
+    
+    paletteBuffer.forEach(decimal => {
+      paletteDecimals.push(decimal);
+    });
+    
+    const newPalette = invertify(paletteDecimals);
+    
+    var newPaletteBuffer = new Buffer(newPalette);
+    
+    var newHeadBuffer = newPaletteBuffer.copy(headBuffer);
+    
+    callback(newHeadBuffer);
+    
+    
 
     fs.writeFile('./images/palette-bitmap-copy.bmp', buf, (err) => {
       if (err) throw err;
@@ -40,5 +48,13 @@ function getBytes(file, callback) {
     });
 
   });
+}
+
+function invertify(inputDecimals) {
+  const invertedDecimals = [];
+  inputDecimals.forEach((decimal, index) => {
+    invertedDecimals[index] = 255 - decimal;
+  });
+  return invertedDecimals;
 }
 
