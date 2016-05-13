1428d6d80b49901aff4e5e11f674b7abc7a5613d
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-13 15:12:22 -0700

    Added writeFile method to create a copy of bitmap file

--- tests/bitmap.test.js
+++ tests/bitmap.test.js
@@ -10,20 +10,33 @@ it('targets image file and logs raw buffer', () => {
 
   getBytes('palette-bitmap.bmp', function(result) {
     console.log(result);
+    var decimalValues = [];
+    result.forEach( (decimal, index) => {
+      decimalValues.push(index + ' ' + decimal);
+    });
+    for (var i = 54; i < 182; i++) {
+      console.log(decimalValues[i]);
+    }
     assert(result);
   });
-    
+
 
 });
 
 function getBytes(file, callback) {
   fs.readFile('./images/' + file, (err, buf) => {
     if (err) throw err;
-    
+
     const offset = buf.readInt16LE(10);
-    const headData = Buffer.from(buf.buffer, 0, offset).toString('binary');
+    const headData = Buffer.from(buf.buffer, 0, offset);
     const bodyData = Buffer.from(buf.buffer, offset);
-    
-    callback(headData);
+
+    // callback(headData);
+
+    fs.writeFile('./images/palette-bitmap-copy.bmp', buf, (err) => {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
+
   });
 }
