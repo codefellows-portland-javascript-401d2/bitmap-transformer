ff7312447696ac9e701430dd6fa285fce6d2e17f
DonChatelain <djchatelain@gmail.com>
2016-05-13 18:04:38 -0700

    adds several colorizing functions that can easily be used

--- tests/bitmap.test.js
+++ tests/bitmap.test.js
@@ -24,7 +24,7 @@ function getBytes(file, callback) {
     const paletteStart = 54;
     const headEnd = buf.readInt16LE(10);
     const headBuffer = Buffer.from(buf.buffer, 0, paletteStart);
-    const paletteBuffer = Buffer.from(buf.buffer, paletteStart, headEnd);
+    const paletteBuffer = Buffer.from(buf.buffer, paletteStart, headEnd - paletteStart);
     const bodyBuffer = Buffer.from(buf.buffer, headEnd);
     const paletteDecimals = [];
     
@@ -32,17 +32,24 @@ function getBytes(file, callback) {
       paletteDecimals.push(decimal);
     });
     
-    const newPalette = invertify(paletteDecimals);
+    // const newPalette = invertify(paletteDecimals);
+    // const newPalette = brighten(paletteDecimals);
+    // const newPalette = darken(paletteDecimals);
+    // const newPalette = funkify(paletteDecimals);
+    const newPalette = bluify(paletteDecimals);
+    // const newPalette = hulkify(paletteDecimals);
     
-    var newPaletteBuffer = new Buffer(newPalette);
+    var newPaletteBuffer = Buffer.from(newPalette);
     
-    var newHeadBuffer = newPaletteBuffer.copy(headBuffer);
+    const totalLength = headBuffer.length + newPaletteBuffer.length - 54 + bodyBuffer.length;
     
-    callback(newHeadBuffer);
+    var outputBuffer = Buffer.concat([headBuffer, newPaletteBuffer, bodyBuffer], totalLength);
+        
+    callback(outputBuffer);
     
     
 
-    fs.writeFile('./images/palette-bitmap-copy.bmp', buf, (err) => {
+    fs.writeFile('./images/palette-bitmap-copy-1.bmp', outputBuffer, (err) => {
       if (err) throw err;
       console.log('It\'s saved!');
     });
@@ -50,6 +57,7 @@ function getBytes(file, callback) {
   });
 }
 
+//Inverts
 function invertify(inputDecimals) {
   const invertedDecimals = [];
   inputDecimals.forEach((decimal, index) => {
@@ -58,3 +66,77 @@ function invertify(inputDecimals) {
   return invertedDecimals;
 }
 
+//Multiplies
+function brighten(inputDecimals) {
+  const delta = 2;
+  const colorizedDecimals = [];
+  inputDecimals.forEach((decimal, index) => {
+    colorizedDecimals[index] = (decimal + 1) * delta;
+    if (colorizedDecimals[index] > 255) {
+      colorizedDecimals[index] = 255;
+    }
+  });
+  return colorizedDecimals;
+}
+
+//
+function darken(inputDecimals) {
+  const delta = 300;
+  const colorizedDecimals = [];
+  inputDecimals.forEach((decimal, index) => {
+    colorizedDecimals[index] = decimal * decimal/delta;
+    if (colorizedDecimals[index] > 255) {
+      colorizedDecimals[index] = 255;
+    }
+  });
+  return colorizedDecimals;
+}
+
+// Get funky
+function funkify(inputDecimals) {
+  const delta = 50;
+  const colorizedDecimals = [];
+  inputDecimals.forEach((decimal, index) => {
+    if (index % 2 === 0) {
+      colorizedDecimals[index] = decimal + delta;
+    }
+    if (colorizedDecimals[index] > 255) {
+      colorizedDecimals[index] = 255;
+    }
+  });
+  return colorizedDecimals;
+}
+
+// I just blue myself
+function bluify(inputDecimals) {
+  const delta = 150;
+  const colorizedDecimals = [];
+  inputDecimals.forEach((decimal, index) => {
+    if (index % 4 === 0) {
+      colorizedDecimals[index] = decimal + delta;
+      if (colorizedDecimals[index] > 255) {
+        colorizedDecimals[index] = 255;
+      }
+    } else {
+      colorizedDecimals[index] = decimal;
+    }
+  });
+  return colorizedDecimals;
+}
+
+// some hulk quote here
+function hulkify(inputDecimals) {
+  const delta = 90;
+  const colorizedDecimals = [];
+  inputDecimals.forEach((decimal, index) => {
+    if (index % 3 === 1) {
+      colorizedDecimals[index] = decimal + delta;
+      if (colorizedDecimals[index] > 255) {
+        colorizedDecimals[index] = 255;
+      }
+    } else {
+      colorizedDecimals[index] = decimal;
+    }
+  });
+  return colorizedDecimals;
+}
